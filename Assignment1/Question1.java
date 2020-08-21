1c5040da4be8884ad659d82bbc31b8e7aef10c1f
Aditya <54849642+aditya31298@users.noreply.github.com>
2020-08-21 23:37:29 +0530

    Update Question1.java

--- Assignment1/Question1.java
+++ Assignment1/Question1.java
@@ -1,1 +1,56 @@
+import java.util.Scanner;
 
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Input string 1:");
+        String s1,s2;
+        s1=scan.nextLine();
+        
+        System.out.println("Input string 2:");
+        s2=scan.next();
+        int k1=s1.length();
+        int k2=s2.length();
+        int z=0,ans=0;
+        if(k1<k2)
+        System.out.println("0");
+        else {
+            int [] a;
+            int [] b;
+            a= new int[500];
+            b=new int[500];
+            for (int i=0;i<k2;i++)
+                 b[(int)s2.charAt(i)]++;
+            for(int i=0;i<k2;i++){
+
+                  int k=(int)s1.charAt(i);
+                  if(a[k]<b[k])
+                    z++;
+                  a[k]++;
+
+                  if(z==k2)
+                    ans++;}
+
+                  for(int i=k2;i<k1;i++){
+
+                  int k=(int)s1.charAt(i);
+                int p=(int)s1.charAt(i-k2);
+
+                if(a[p]<=b[p])
+                z--;
+
+                a[p]--;
+
+                if(a[k]<b[k])
+                z++;
+
+                a[k]++;
+
+                if(z==k2)
+                ans++;}
+
+                System.out.println(ans);        
+        }
+            
+    }
+}
